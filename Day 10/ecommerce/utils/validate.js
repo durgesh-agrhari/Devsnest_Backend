0cbe4ddbbaec556773f3b7f87e9a8a4b340e99c4
durgesh-agrhari <65119161+durgesh-agrhari@users.noreply.github.com>
2021-09-30 19:58:17 +0530

    Add files via upload

---
+++ Day 10/ecommerce/utils/validate.js
@@ -0,0 +1,17 @@
+//email verif regex
+const emailValidate = (email) => {
+  const re =
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(email);
+  // return re.test(String(email).toLowerCase());
+};
+
+const passwordValidate = (password) => {
+  var re = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/;
+  return re.test(password);
+};
+
+module.exports = {
+  emailValidate,
+  passwordValidate,
+};
