c0725c9b12948bf7103258d987dda797bb6aa072
durgesh-agrhari <65119161+durgesh-agrhari@users.noreply.github.com>
2021-10-12 21:47:28 +0530

    Delete Day14-17 directory

--- Day14-17/middlewares/checkSuperAdmin.js
+++
@@ -1,9 +0,0 @@
-const check = (req, res, next) => {
-    if (req.session.User.role === "Super-admin") {
-      next();
-    } else {
-      res.status(401).send("Needs to be super admin");
-    }
-  };
-  
-  module.exports = check;
