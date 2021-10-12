bddacff20d492dcb9d57944041076910f39f4bcf
durgesh-agrhari <65119161+durgesh-agrhari@users.noreply.github.com>
2021-10-12 21:46:44 +0530

    Add files via upload

---
+++ Day14-17/middlewares/passport.js
@@ -0,0 +1,27 @@
+const User = require("../models/mongo");
+const { SECRET } = require("../config");
+const { Strategy, ExtractJwt } = require("passport-jwt");
+
+const opts = {
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+  secretOrKey: SECRET,
+};
+
+module.exports = (passport) => {
+  passport.use(
+    new Strategy(opts, async (payload, done) => {
+      await User.findById(payload.user_id)
+        .then((user) => {
+          if (user) {
+            return done(null, user); //next
+          } else {
+            return done(null, false);
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+          return done(null, false);
+        });
+    })
+  );
+};
